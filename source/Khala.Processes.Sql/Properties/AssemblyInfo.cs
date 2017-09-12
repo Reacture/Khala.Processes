f6e540b1ddeb62531f8696c06a3f2e1663a8294a
Gyuwon <gyuwon@live.com>
2017-09-12 23:59:20 +0900

    Khala.Processes.Sql 프로젝트를 추가합니다.

---
+++ source/Khala.Processes.Sql/Properties/AssemblyInfo.cs
@@ -0,0 +1,11 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyProduct("Khala.Processes")]
+[assembly: AssemblyTitle("Khala.Processes.Sql")]
+[assembly: AssemblyCompany("Reacture")]
+[assembly: AssemblyCopyright("Copyright Gyuwon 2017")]
+[assembly: ComVisible(false)]
+[assembly: Guid("aaa2bae1-441c-46bb-a2ed-50eb8e233031")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
