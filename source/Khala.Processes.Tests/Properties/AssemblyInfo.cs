3e1460ff1a9c102e2f819f044a261fe591620cd1
Gyuwon <gyuwon@live.com>
2017-09-12 23:54:14 +0900

    Khala.Processes.Tests 프로젝트를 추가합니다.

---
+++ source/Khala.Processes.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,7 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: ComVisible(false)]
+[assembly: Guid("64e14508-24d4-4c95-ae02-e7cbe49b24dd")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
