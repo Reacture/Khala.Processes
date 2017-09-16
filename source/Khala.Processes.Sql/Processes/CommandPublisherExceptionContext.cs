61a928921c240d815164880c7be158b7dba93f48
Gyuwon <gyuwon@live.com>
2017-09-16 22:51:27 +0900

    CommandPublisherExceptionContext 클래스에 주석을 추가합니다.

--- source/Khala.Processes.Sql/Processes/CommandPublisherExceptionContext.cs
+++ source/Khala.Processes.Sql/Processes/CommandPublisherExceptionContext.cs
@@ -2,8 +2,17 @@
 {
     using System;
 
+    /// <summary>
+    /// Encapsulates information about an error that occur while publishing commands associated with a process manager.
+    /// </summary>
     public class CommandPublisherExceptionContext
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CommandPublisherExceptionContext"/> class.
+        /// </summary>
+        /// <param name="processManagerType">The type of the process manager.</param>
+        /// <param name="processManagerId">The identifier of the process manager.</param>
+        /// <param name="exception">The caught exception.</param>
         public CommandPublisherExceptionContext(
             Type processManagerType,
             Guid processManagerId,
@@ -19,12 +28,28 @@
             Exception = exception ?? throw new ArgumentNullException(nameof(exception));
         }
 
+        /// <summary>
+        /// Gets the type of the process manager.
+        /// </summary>
+        /// <value>The type of the process manager.</value>
         public Type ProcessManagerType { get; }
 
+        /// <summary>
+        /// Gets the identifier of the process manager.
+        /// </summary>
+        /// <value>The identifier of the process manager.</value>
         public Guid ProcessManagerId { get; }
 
+        /// <summary>
+        /// Gets the caught exception.
+        /// </summary>
+        /// <value>The caught exception.</value>
         public Exception Exception { get; }
 
+        /// <summary>
+        /// Gets or sets a value indicating whether the caught exception have been handled so it may not be rethrown.
+        /// </summary>
+        /// <value>A value indicating whether the caught exception have been handled so it may not be rethrown.</value>
         public bool Handled { get; set; } = false;
     }
 }
