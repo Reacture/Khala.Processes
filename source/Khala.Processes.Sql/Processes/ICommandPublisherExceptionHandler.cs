43267fc9c1bdf33755f9e7cfe941bc76fc586cde
Gyuwon <gyuwon@live.com>
2017-09-16 23:02:53 +0900

    ICommandPublisherExceptionHandler 인터페이스에 주석을 추가합니다.

--- source/Khala.Processes.Sql/Processes/ICommandPublisherExceptionHandler.cs
+++ source/Khala.Processes.Sql/Processes/ICommandPublisherExceptionHandler.cs
@@ -2,8 +2,16 @@
 {
     using System.Threading.Tasks;
 
+    /// <summary>
+    /// Represents an exception handler handling exceptions that occur while publishing process manager commands.
+    /// </summary>
     public interface ICommandPublisherExceptionHandler
     {
+        /// <summary>
+        /// Handles an exception that occurred while publishing process manager commands.
+        /// </summary>
+        /// <param name="context">A <see cref="CommandPublisherExceptionContext"/> contains information about the exception that occurred.</param>
+        /// <returns>A task representing the asynchronous operation.</returns>
         Task Handle(CommandPublisherExceptionContext context);
     }
 }
