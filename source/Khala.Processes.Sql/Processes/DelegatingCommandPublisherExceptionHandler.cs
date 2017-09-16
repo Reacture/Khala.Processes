cf1358567fae6f1d222f60c706a60067ae3c5259
Gyuwon <gyuwon@live.com>
2017-09-16 23:10:15 +0900

    DelegatingCommandPublisherExceptionHandler 클래스에 주석을 추가합니다.

--- source/Khala.Processes.Sql/Processes/DelegatingCommandPublisherExceptionHandler.cs
+++ source/Khala.Processes.Sql/Processes/DelegatingCommandPublisherExceptionHandler.cs
@@ -3,16 +3,27 @@
     using System;
     using System.Threading.Tasks;
 
+    /// <summary>
+    /// An implementation of <see cref="ICommandPublisherExceptionHandler"/> that delegates responsibility for handling exceptions to a handler function.
+    /// </summary>
     public class DelegatingCommandPublisherExceptionHandler : ICommandPublisherExceptionHandler
     {
         private Func<CommandPublisherExceptionContext, Task> _handler;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DelegatingCommandPublisherExceptionHandler"/> class with an asynchronous exception handler function.
+        /// </summary>
+        /// <param name="handler">An asynchronous exception handler function.</param>
         public DelegatingCommandPublisherExceptionHandler(
             Func<CommandPublisherExceptionContext, Task> handler)
         {
             _handler = handler ?? throw new ArgumentNullException(nameof(handler));
         }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DelegatingCommandPublisherExceptionHandler"/> class with a synchronous exception handler function.
+        /// </summary>
+        /// <param name="handler">A synchronous exception handler function.</param>
         public DelegatingCommandPublisherExceptionHandler(
             Action<CommandPublisherExceptionContext> handler)
         {
@@ -28,6 +39,11 @@
             };
         }
 
+        /// <summary>
+        /// Handles an exception that occurred while publishing process manager commands.
+        /// </summary>
+        /// <param name="context">A <see cref="CommandPublisherExceptionContext"/> contains information about the exception that occurred.</param>
+        /// <returns>A task representing the asynchronous operation.</returns>
         public Task Handle(CommandPublisherExceptionContext context)
         {
             if (context == null)
