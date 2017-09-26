271a30691d466a3c10048265cf59d54df10cf693
Gyuwon <gyuwon@live.com>
2017-09-26 11:22:01 +0900

    CompositeCommandPublisherExceptionHandler 클래스를 추가합니다.

---
+++ source/Khala.Processes.Sql/Processes/CompositeCommandPublisherExceptionHandler.cs
@@ -0,0 +1,59 @@
+﻿namespace Khala.Processes
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Diagnostics;
+    using System.Threading.Tasks;
+
+    public class CompositeCommandPublisherExceptionHandler
+    {
+        private readonly IEnumerable<ICommandPublisherExceptionHandler> _handlers;
+
+        public CompositeCommandPublisherExceptionHandler(params ICommandPublisherExceptionHandler[] handlers)
+        {
+            if (handlers == null)
+            {
+                throw new ArgumentNullException(nameof(handlers));
+            }
+
+            var handlerList = new List<ICommandPublisherExceptionHandler>(handlers);
+            for (int i = 0; i < handlerList.Count; i++)
+            {
+                ICommandPublisherExceptionHandler handler = handlerList[i];
+                if (handler == null)
+                {
+                    throw new ArgumentException(
+                        $"{nameof(handlers)} cannot contain null.",
+                        nameof(handlers));
+                }
+            }
+
+            _handlers = handlerList;
+        }
+
+        public Task Handle(CommandPublisherExceptionContext context)
+        {
+            if (context == null)
+            {
+                throw new ArgumentNullException(nameof(context));
+            }
+
+            return RunHandle(context);
+        }
+
+        private async Task RunHandle(CommandPublisherExceptionContext context)
+        {
+            foreach (ICommandPublisherExceptionHandler handler in _handlers)
+            {
+                try
+                {
+                    await handler.Handle(context).ConfigureAwait(false);
+                }
+                catch (Exception handlerError)
+                {
+                    Trace.TraceError(handlerError.ToString());
+                }
+            }
+        }
+    }
+}
---
+++ source/Khala.Processes.Tests/Processes/CompositeCommandPublisherExceptionHandler_specs.cs
@@ -0,0 +1,91 @@
+﻿namespace Khala.Processes
+{
+    using System;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using FluentAssertions;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Moq;
+    using Ploeh.AutoFixture;
+    using Ploeh.AutoFixture.AutoMoq;
+    using Ploeh.AutoFixture.Idioms;
+
+    [TestClass]
+    public class CompositeCommandPublisherExceptionHandler_specs
+    {
+        [TestMethod]
+        public async Task Handle_invokes_all_inner_handler_functions()
+        {
+            // Arrange
+            ICommandPublisherExceptionHandler[] handlers = new[]
+            {
+                Mock.Of<ICommandPublisherExceptionHandler>(),
+                Mock.Of<ICommandPublisherExceptionHandler>(),
+                Mock.Of<ICommandPublisherExceptionHandler>()
+            };
+            var fixture = new Fixture();
+            var context = fixture.Create<CommandPublisherExceptionContext>();
+            var sut = new CompositeCommandPublisherExceptionHandler(handlers);
+
+            // Act
+            await sut.Handle(context);
+
+            // Assert
+            foreach (ICommandPublisherExceptionHandler handler in handlers)
+            {
+                Mock.Get(handler).Verify(x => x.Handle(context), Times.Once());
+            }
+        }
+
+        [TestMethod]
+        public void sut_has_guard_clauses()
+        {
+            var builder = new Fixture().Customize(new AutoMoqCustomization());
+            new GuardClauseAssertion(builder).Verify(typeof(CompositeCommandPublisherExceptionHandler));
+        }
+
+        [TestMethod]
+        public void constructor_has_guard_clause_against_null_handler()
+        {
+            ICommandPublisherExceptionHandler[] handlers = new[]
+            {
+                default(ICommandPublisherExceptionHandler),
+                Mock.Of<ICommandPublisherExceptionHandler>(),
+                Mock.Of<ICommandPublisherExceptionHandler>()
+            };
+            var random = new Random();
+
+            Action action = () =>
+            new CompositeCommandPublisherExceptionHandler(handlers.OrderBy(h => random.Next()).ToArray());
+
+            action.ShouldThrow<ArgumentException>().Where(x => x.ParamName == "handlers");
+        }
+
+        [TestMethod]
+        public void Handle_absorbs_handler_exception()
+        {
+            // Arrange
+            ICommandPublisherExceptionHandler[] handlers = new[]
+            {
+                Mock.Of<ICommandPublisherExceptionHandler>(),
+                Mock.Of<ICommandPublisherExceptionHandler>(),
+                Mock.Of<ICommandPublisherExceptionHandler>()
+            };
+
+            var fixture = new Fixture();
+            var context = fixture.Create<CommandPublisherExceptionContext>();
+            Mock.Get(handlers[0])
+                .Setup(x => x.Handle(context))
+                .ThrowsAsync(new InvalidOperationException());
+
+            var random = new Random();
+            var sut = new CompositeCommandPublisherExceptionHandler(handlers.OrderBy(h => random.Next()).ToArray());
+
+            // Act
+            Func<Task> action = () => sut.Handle(context);
+
+            // Assert
+            action.ShouldNotThrow();
+        }
+    }
+}
