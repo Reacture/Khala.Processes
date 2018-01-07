765d4d676920b18d5dff8f00a2b3f497d6fcc79b
Gyuwon Yi <gyuwon@live.com>
2018-01-07 11:55:35 +0900

    versioning 0.9.0

--- source/Khala.Processes.Sql/Properties/AssemblyInfo.cs
+++ source/Khala.Processes.Sql/Properties/AssemblyInfo.cs
@@ -7,5 +7,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("Copyright Gyuwon 2017")]
 [assembly: ComVisible(false)]
 [assembly: Guid("aaa2bae1-441c-46bb-a2ed-50eb8e233031")]
-[assembly: AssemblyVersion("0.8.0.0")]
-[assembly: AssemblyFileVersion("0.8.0.0")]
+[assembly: AssemblyVersion("0.9.0.0")]
+[assembly: AssemblyFileVersion("0.9.0.0")]
